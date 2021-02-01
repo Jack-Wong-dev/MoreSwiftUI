f623a6d370d8d7155317e5c91ddab289e359475c
Jack Wong <jackwong@professional.com>
2021-01-31 20:54:43 -0500

    Changed the Unwrap internal init into public init.

--- Sources/MoreSwiftUI/Conditionals/Unwrap/Unwrap.swift
+++ Sources/MoreSwiftUI/Conditionals/Unwrap/Unwrap.swift
@@ -18,7 +18,7 @@ public struct Unwrap<Value, Content: View>: View {
     private let value: Value?
     private let contentProvider: (Value) -> Content
     
-    init(_ value: Value?,
+    public init(_ value: Value?,
          @ViewBuilder content: @escaping (Value) -> Content) {
         self.value = value
         self.contentProvider = content
